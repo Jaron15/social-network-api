295be77fc8c3aa91f0b56adb8e43147f67bede64
Jaron <jjkenyon777@gmail.com>
2021-12-04 19:35:38 -0700

    get thought routes added

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,41 @@
 const {  User, Thought } = require('../models');
 
 const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({ 
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+        .populate({ 
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+
     addThought({ params, body }, res) {
         console.log(body);
         Thought.create(body)
@@ -67,8 +102,9 @@ const thoughtController = {
             }
             res.json(dbUserData);
         })
-        .catch(dbUserData)
+      
     }
 };
 
 module.exports = thoughtController;
+
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 const { 
+    getAllThoughts,
+    getThoughtById,
     addThought,
     removeThought,
     addReaction,
     removeReaction } = require('../../controllers/thought-controller');
 
+router
+.route('/')
+.get(getAllThoughts)
+
 router.route('/:userId').post(addThought);
 
-
 router.route('/:userId/:thoughtId')
+.get(getThoughtById)
 .put(addReaction)
 .delete(removeThought);
 
